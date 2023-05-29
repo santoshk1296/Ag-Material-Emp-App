3b0ff622ea16058406d87528af94237d2f9c2793
santoshk1296 <santoshk1296@gmail.com>
2023-05-29 15:53:20 +0530

    New Commit

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -11,7 +11,8 @@ export class EmployeeService {
 
   private errorMsg: string = "";
 
-  private baseUrl = 'http://localhost:8080/emp-api/v1/employees';
+  private baseUrl = 'http://localhost:8091/emp-api/v1/employees';
+  //private baseUrl = 'http://192.168.0.165:5000/emp-api/v1/employees'; //Kubernetes cluster url on Macbook CentOs VMware
 
   constructor(private httpclient: HttpClient) { }
 
