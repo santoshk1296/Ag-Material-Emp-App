0b250428d97369fc645c6c32d2a9cc75c981ca5f
santoshk1296 <santoshk1296@gmail.com>
2023-09-15 11:12:24 +0530

    All file

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -12,7 +12,7 @@ export class EmployeeService {
   private errorMsg: string = "";
 
   //private baseUrl = 'http://localhost:8091/emp-api/v1/employees';
-  private baseUrl = 'http://emplyee-mgmt-api:5000/emp-api/v1/employees'; //Kubernetes cluster url on Macbook CentOs VMware
+  private baseUrl = `http://${process.env.EMPLOYEE_MGMT_API_HOST}/emp-api/v1/employees`; //Kubernetes cluster url on Macbook CentOs VMware
   //private baseUrl ='http://empmgmtbackend.centralindia.cloudapp.azure.com:8091/emp-api/v1/employees'; //Kubernetes cluster url on Azure AKS
   //Url to launch : http://192.168.0.166:5001/
   
